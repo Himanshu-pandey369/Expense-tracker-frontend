274561544ddc333fbee3e0c3ff187dbddcffc2eb
Himanshu <hp1981388@gmail.com>
2026-07-26 10:16:11 +0530

    Fixed import issue

--- Expense-Tracker_frontend/src/main.jsx
+++ Expense-Tracker_frontend/src/main.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import {ThemeProvider} from "./Context/ThemeContext"
+import { ThemeProvider } from "./Context/ThemeContext";
 import App from "./App";
 import "./index.css";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./Context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
